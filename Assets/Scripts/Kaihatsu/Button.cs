d166b8ba4cf3a95b92e3f216fdbfc9e0b7c59983
DA\nwuser <noppo.kappa@gmail.com>
2017-06-26 09:40:15 +0900

    ◆仮UI(ボタン)設置 ◆仮UI(歯車)設置

---
+++ Assets/Scripts/Kaihatsu/Button.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button : MonoBehaviour
+{
+
+
+    bool click_flg = false; //ボタンがクリックされているか
+    bool isBotton;
+    SpriteRenderer SpriteButton;
+
+    public Sprite OnButton;     //赤いボタン(ON)
+    public Sprite OffButton;    //青いボタン(OFF)
+
+
+    // Rayの当たったオブジェクトの情報を格納する
+    RaycastHit hit = new RaycastHit();
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        StandBy();
+        b_click();
+
+    }
+
+    void b_click(){
+
+       
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Vector2 clickPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);            
+
+            if (Physics2D.OverlapPoint(clickPos))
+            {
+                RaycastHit2D hitInfo = Physics2D.Raycast(clickPos, -Vector2.up,
+                    float.MaxValue,LayerMask.NameToLayer("Button"));
+
+                click_flg = true;
+                //クリックされてる時ONにチェンジ
+                SpriteButton.sprite = OnButton;
+                Debug.Log("on" + click_flg);
+
+            }
+            //ボタンからマウスが出てるときはOFF.
+            else
+            {
+                click_flg = false;
+                //放されたときOFFにチェンジ
+                SpriteButton.sprite = OffButton;
+                Debug.Log("off" + click_flg);
+            }
+
+        }
+       
+    }
+
+
+    void StandBy() {
+        SpriteButton = gameObject.GetComponent<SpriteRenderer>();    
+    }
+
+   
+}
+
+    
+
