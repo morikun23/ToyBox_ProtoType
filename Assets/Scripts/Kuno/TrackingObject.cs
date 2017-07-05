6f96a52b7becff3f9a625ddaed048b7a967cd644
naonosuke8267 <naonosuke8267@gmail.com>
2017-07-05 17:30:24 +0900

    移動床の上で滑らないの実装
    プレーヤーの摩擦Maxにして、Velocityで動く床と移動が完全同期するようにしつつ、
    プレーヤー自体はTranslateで動くことでとりあえず実装。

--- Assets/Scripts/Kuno/TrackingObject.cs
+++ Assets/Scripts/Kuno/TrackingObject.cs
@@ -8,12 +8,11 @@ public class TrackingObject : MonoBehaviour {
 	public Vector3 m_pos_colNew;
 
 	private float m_num_distance;
-	//private GameObject m_obj_collider;
 
 	bool m_flg_hit = false;
 	private Collision2D m_col_object;
 
-	private List<TrackingObject> m_spr_TrackingObject = new List<TrackingObject>();
+	private TrackingObject m_spr_TrackingObject;
 
 	void Start(){
 		m_pos_colBefore = Vector3.zero;
@@ -30,58 +29,34 @@ public class TrackingObject : MonoBehaviour {
 
 	void Update(){
 
-		m_pos_colBefore = m_pos_colNew;
-		m_pos_colNew = transform.position;
-
-		if(m_enu_mode == Mode.TrackingRecive || m_enu_mode == Mode.Both){
-			if (m_flg_hit) {
-
-				for(int i = 0;i < m_spr_TrackingObject.Count - 1;i ++){
-
-					m_num_distance = transform.position.y - m_col_object.contacts [i].point.y;
-
-					if (m_spr_TrackingObject[i].m_pos_colBefore != m_spr_TrackingObject[i].m_pos_colNew &&
-						m_num_distance > 0) {
-
-//						transform.position = new Vector3 (
-//							transform.position.x + (m_spr_TrackingObject[i].m_pos_colNew.x - m_spr_TrackingObject[i].m_pos_colBefore.x),
-//							transform.position.y + (m_spr_TrackingObject[i].m_pos_colNew.y - m_spr_TrackingObject[i].m_pos_colBefore.y),
-//							0
-//						);
-
-						transform.Translate (new Vector3 (
-							(m_spr_TrackingObject[i].m_pos_colNew.x - m_spr_TrackingObject[i].m_pos_colBefore.x) * 1.3f,
-							m_spr_TrackingObject[i].m_pos_colNew.y - m_spr_TrackingObject[i].m_pos_colBefore.y,
-							0
-						));
-					}
-				}
-			}
-
-			m_spr_TrackingObject.Clear();
-		}
+//		m_pos_colBefore = m_pos_colNew;
+//		m_pos_colNew = transform.position;
+//
+//		if(m_enu_mode == Mode.TrackingRecive || m_enu_mode == Mode.Both){
+//			if (m_flg_hit) {
+//
+//				for(int i = 0;i < m_spr_TrackingObject.Count - 1;i ++){
+//
+//					transform.parent = m_spr_TrackingObject [i].transform;
+//				}
+//			}
+//
+//			m_spr_TrackingObject.Clear();
+//		}
 	}
 
-	void OnCollisionStay2D(Collision2D col){
-		m_col_object = col;
-
-		int baf_cnt = 0;
-
-		for (int i = 0; i < m_col_object.contacts.Length; i++) {
-			TrackingObject baf_obj;
-
-			if(baf_obj = m_col_object.contacts [i].collider.gameObject.GetComponent<TrackingObject> ()){
-				if(baf_obj.m_enu_mode == Mode.TrackingSend || baf_obj.m_enu_mode == Mode.Both){
-					baf_cnt++;
-					m_flg_hit = true;
-					m_spr_TrackingObject.Add(baf_obj);
-				}
-			}
+	void OnCollisionEnter2D(Collision2D col){
+		if ((m_enu_mode == Mode.TrackingRecive || m_enu_mode == Mode.Both) &&
+			col.gameObject.GetComponent<TrackingObject>().m_enu_mode == Mode.TrackingSend) {
+			m_col_object = col;
+			transform.parent = col.transform;
 		}
 	}
 
 	void OnCollisionExit2D(Collision2D col){
-		m_flg_hit = false;
+
+		//m_flg_hit = false;
 		m_col_object = null;
+		transform.parent = null;
 	}
 }
