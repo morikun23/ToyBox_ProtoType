4b3555c59d7547ca2ae48f648cb36105b37e71c4
oyama <oyameycry3024@gmail.com>
2017-07-13 10:35:37 +0900

    【リザルトシーンにフェード追加】
    ・その名のとおり

--- Assets/Scripts/Oyama/ResultManager.cs
+++ Assets/Scripts/Oyama/ResultManager.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class ResultManager : MonoBehaviour {
 
+    //フェード用の板
+    public SpriteRenderer m_filter;
+
+    //フェード用の減衰値
+    public float m_alphaNum = 0.015f;
+
 	// Use this for initialization
 	void Start () {
-		
+
+        m_filter.color = new Color(0, 0, 0, 1);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        if (m_filter.color.a > 0) m_filter.color -= new Color(0, 0, 0, m_alphaNum);
 
         if (Input.touchCount < 1) return;
         if(Input.GetTouch(0).phase == TouchPhase.Began)
