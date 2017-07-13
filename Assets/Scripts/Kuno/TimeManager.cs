9e57d752f028a9744347c86d727459f876a2507b
naonosuke8267 <naonosuke8267@gmail.com>
2017-07-13 09:23:13 +0900

    追記：コミットできました

---
+++ Assets/Scripts/Kuno/TimeManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour {
+
+	[Range(0,2)]
+	static public float m_num_timeScale = 1;
+
+	public enum Status{
+		neutoral,
+		slow,
+		stop
+	}
+
+	static public Status enu_status;
+	private bool flg_stop;
+
+	public List<Vector2> m_num_velocity = new List<Vector2>();
+	public List<float> m_num_gravity = new List<float>();
+	public List<GameObject> m_obj = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Time.timeScale = m_num_timeScale;
+		Time.fixedDeltaTime = m_num_timeScale * 0.02f;
+
+		if (m_num_timeScale != 1) {
+			if(flg_stop == true){
+				enu_status = Status.stop;
+			}
+			enu_status = Status.slow;
+		} else {
+			enu_status = Status.neutoral;
+		}
+	}
+
+	public void TimeStop(){
+		flg_stop = true;
+	}
+
+	public void TimeStart(){
+		flg_stop = false;
+	}
+}
