1f87a22703b318cc6b1d1a36a0d74ed8391fa146
naonosuke8267 <naonosuke8267@gmail.com>
2017-07-18 13:54:31 +0900

    プレーヤーの挙動修正中

--- Assets/Scripts/Kuno/K_Swipe.cs
+++ Assets/Scripts/Kuno/K_Swipe.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class K_Swipe : MonoBehaviour {
 
-	Vector3 pos_input;
+	Vector3[] pos_input = new Vector3[10];
 	Vector3 pos_inputScreen;
-	Vector3 pos_inputBefore;
-	Vector3 pos_init;
+	Vector3[] pos_inputBefore = new Vector3[10];
+	Vector3[] pos_init = new Vector3[10];
 	Vector3 num_size;
 
 	public Transform pos_rotatePoint;
+
+	Vector2 pos_screenBefore;
+	Vector2 pos_screenNew;
 
 	bool[] flg_start = new bool[10];
 	bool[] flg_move = new bool[10];
@@ -35,58 +38,64 @@ public class K_Swipe : MonoBehaviour {
 	
 	// Update is called once per frame
 	public void Update () {
-		//１フレーム前のタッチ座標を格納
-		pos_inputBefore = pos_input;
+		pos_screenBefore = pos_screenNew;
+		pos_screenNew = Camera.main.transform.position;
 
 		int baf_i = 0;
 		foreach (Touch t in Input.touches) {
 			baf_i++;
 			//タッチ位置を取得
+			//１フレーム前のタッチ座標を格納
+			pos_inputBefore[baf_i] = pos_input[baf_i];
 			pos_inputScreen = t.position;
-			pos_input = Camera.main.ScreenToWorldPoint (t.position);
-			switch(t.phase){
-			case TouchPhase.Began:
-				if (!CheckHitPoint (pos_input))
-					continue;
-				Started ();
-				pos_init = pos_input;
-				flg_start[baf_i] = true;
-				//Debug.Log ("START");
-				break;
-			
-			case TouchPhase.Moved:
-				if (!flg_start [baf_i])
-					continue;
-				//Debug.Log ("FINGER MOVEING");
-				Moving (t.deltaPosition);
-				CheckRotate (t,baf_i);
-				flg_move [baf_i] = true;
-				break;
-			
-			case TouchPhase.Ended:
-				if (!flg_start [baf_i])
-					continue;
+			pos_input[baf_i] = Camera.main.ScreenToWorldPoint (t.position);
+				switch(t.phase){
+				case TouchPhase.Began:
+					if (!CheckHitPoint (pos_input[baf_i]))
+						continue;
+					Started ();
+					pos_init[baf_i] = pos_input[baf_i];
+					flg_start[baf_i] = true;
+					//Debug.Log ("START");
+					break;
+				
+				case TouchPhase.Moved:
+					if (!flg_start [baf_i])
+						continue;
+					//Debug.Log ("FINGER MOVEING");
+					Moving (t.deltaPosition);
+					CheckRotate (t,baf_i);
+					flg_move [baf_i] = true;
+					break;
+				
+				case TouchPhase.Ended:
+					if (!flg_start [baf_i])
+						continue;
 
-				if (!CheckMove ()) {
-					SwipeE ();
-					//Debug.Log ("SWIPE END");
-				} else {
-					TouchE ();
-					//Debug.Log ("TOUCH END");
+					if (!CheckMove (baf_i)) {
+						SwipeE ();
+						//Debug.Log ("SWIPE END");
+					} else {
+						TouchE ();
+						//Debug.Log ("TOUCH END");
+					}
+					flg_start [baf_i] = false;
+					flg_move [baf_i] = false;
+					num_rotateDirection [baf_i] = 0;
+
+					pos_input [baf_i] = Vector3.zero;
+					pos_inputBefore [baf_i] = Vector3.zero;
+					break;
 				}
-				flg_start [baf_i] = false;
-				flg_move [baf_i] = false;
-				num_rotateDirection [baf_i] = 0;
-				break;
-			}
-			if (flg_move[baf_i] && flg_start[baf_i]) {
-				if(num_rotateDirection[baf_i] == 1){
-					RightR (1);
-				}else if(num_rotateDirection[baf_i] == -1){
-					LeftR (-1);
+
+				if (flg_move[baf_i] && flg_start[baf_i]) {
+					if(num_rotateDirection[baf_i] == 1){
+						RightR (1);
+					}else if(num_rotateDirection[baf_i] == -1){
+						LeftR (-1);
+					}
+					break;
 				}
-				break;
-			}
 		}
 	}
 
@@ -100,31 +109,28 @@ public class K_Swipe : MonoBehaviour {
 	}
 
 	//タッチの許容範囲かどうかの計測
-	bool CheckMove(){
-		if(Mathf.Abs(pos_input.x - pos_init.x) <= 1 &&
-			Mathf.Abs(pos_input.y - pos_init.y) <= 1){
+	bool CheckMove(int i){
+		if(Mathf.Abs(pos_input[i].x - pos_init[i].x) <= 1 &&
+			Mathf.Abs(pos_input[i].y - pos_init[i].y) <= 1){
 			return true;
 		}
 		return false;
 	}
 
 	void CheckRotate(Touch t,int i){
-		Vector3 baf_pos = Camera.main.WorldToScreenPoint(pos_rotatePoint.position);
-		float baf_rotBef = Vector3.Angle (baf_pos,Camera.main.WorldToScreenPoint(pos_inputBefore));
-		float baf_rotNew = Vector3.Angle (baf_pos,Camera.main.WorldToScreenPoint(pos_input));
+		Vector2 baf_pos = Camera.main.WorldToScreenPoint(pos_rotatePoint.position);
+		float baf_rotBef = Mathf.Atan2 (baf_pos.y - Camera.main.WorldToScreenPoint(pos_inputBefore[i]).y,
+			baf_pos.x - Camera.main.WorldToScreenPoint(pos_inputBefore[i]).x) * Mathf.Rad2Deg;
+		float baf_rotNew = Mathf.Atan2 (baf_pos.y - Camera.main.WorldToScreenPoint(pos_input[i]).y + (pos_screenNew.y - pos_screenBefore.y),
+			baf_pos.x - Camera.main.WorldToScreenPoint(pos_inputBefore[i]).x) * Mathf.Rad2Deg;
 
-		Debug.Log (baf_rotNew - baf_rotBef);
+
 
 		if(baf_rotNew - baf_rotBef > 2){
-			//RightR (baf_rotNew - baf_rotBef);
 			num_rotateDirection[i] = 1;
-
-			//Debug.Log ("RIGIT RORATE");
 		}
 		else if(baf_rotNew - baf_rotBef < -2){
-			//LeftR (baf_rotNew - baf_rotBef);
 			num_rotateDirection[i] = -1;
-			//Debug.Log ("LEFT RORATE");
 		}
 	}
 
